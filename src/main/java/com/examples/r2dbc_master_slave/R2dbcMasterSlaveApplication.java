d0e42d08302b08ddbfae6d2e368cc7ca70aeba8d
Devashish Taneja <dtaneja@enphaseenergy.com>
2023-09-27 21:27:31 +0530

    Basic application

---
+++ src/main/java/com/examples/r2dbc_master_slave/R2dbcMasterSlaveApplication.java
@@ -0,0 +1,13 @@
+package com.examples.r2dbc_master_slave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R2dbcMasterSlaveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(R2dbcMasterSlaveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/examples/r2dbc_master_slave/R2dbcMasterSlaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examples.r2dbc_master_slave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class R2dbcMasterSlaveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
